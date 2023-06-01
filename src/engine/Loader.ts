f82223461045798995d2089af6acf308557e642d
Aleksandr Kadimov <alexander.kadymov@vk.team>
2023-06-01 20:11:54 +0300

    Added loader for tiles

--- src/engine/Loader.ts
+++ src/engine/Loader.ts
@@ -1,5 +1,5 @@
 export interface ImgResDeclaration {
-  [objName: string]: string | string[]
+  [objName: string]: string | string[] | {url: string, tileWidth: number, tileHeight: number}
 }
 
 export interface ImgResOutput {
@@ -19,6 +19,28 @@ const loadImagesArray = (urls: string[]): Promise<ImageBitmap[]> => {
   return Promise.all(urls.map(loadImage))
 }
 
+const loadTileset = async (url: string, tileWidth: number, tileHeight: number) => {
+  const image = await loadImage(url);
+  const canvas = document.createElement('canvas');
+  canvas.width = tileWidth;
+  canvas.height = tileHeight;
+  const ctx = canvas.getContext('2d');
+
+  if (!ctx) return [];
+
+  const imagesArray: ImageBitmap[] = [];
+
+  for (let y = 0; y < image.height; y += tileHeight) {
+    for (let x = 0; x < image.width; x += tileWidth) {
+      ctx.clearRect(0, 0, tileWidth, tileHeight);
+      ctx.drawImage(image, x, y, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
+      imagesArray.push(await createImageBitmap(canvas));
+    }
+  }
+
+  return imagesArray;
+}
+
 export class Loader {
   static async loadImages(
     imgUrlObj: ImgResDeclaration
@@ -26,12 +48,16 @@ export class Loader {
     const result: ImgResOutput = {};
 
     for (const name of Object.keys(imgUrlObj)) {
-      const urls = imgUrlObj[name];
-      const urlsArr: string[] = Array.isArray(urls)
-        ? urls
-        : [urls];
+      const source = imgUrlObj[name];
 
-      result[name] = await loadImagesArray(urlsArr);
+      if (Array.isArray(source)) {
+        result[name] = await loadImagesArray(source);
+      } else if (typeof source === 'string') {
+        result[name] = [await loadImage(source)];
+      } else {
+        const {url, tileWidth, tileHeight} = source;
+        result[name] = await loadTileset(url, tileWidth, tileHeight);
+      }
     }
 
     return result;
